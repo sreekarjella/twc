8f36236b0f9a1f884b9cba24362e12d31aeb200c
sree1508 <142993916+sree1508@users.noreply.github.com>
2024-10-14 22:25:43 +0530

    directory change

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Helmet } from "react-helmet";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+<div>
+      <Helmet>
+        <title>TWC Architects</title>
+      </Helmet>
+      <h1>Welcome The White walls company</h1>
+    </div>
+      {}
+    </div>
+  );
+}
+
+export default App;
